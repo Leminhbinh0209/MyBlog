f6e63ed946be22a9454b488f61b825f10e9f43d4
Leminhbinh0209 <binhleminh0209@gmail.com>
2020-07-07 00:32:47 -0600

    move private infor into dev.js

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://minhbinhle:lmb02091996@myblog.jx0qh.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser:true}).then(() => console.log("DB connected OK")).catch(err => console.log(err));
+const bodyParser = require('body-parser');
+const cookieParser = require ('cookie-parser');
+
+const config = require('./config/key')
+
+const {User} = require('./models/user');
+
+mongoose.connect(config.mongoURI, {useNewUrlParser:true}).then(() => console.log("DB connected OK")).catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if (err) return res.json({success:false, err});
+        return res.status(200).json({
+            success: true,
+            userData: doc
+        });
+    });
+    
+})
 
 app.get('/', (req, res) => {
-    res.send("Hello world");
+    res.send("Hello world!");
 });
 app.listen(5000, () =>{
     console.log("Server running at port 5000")
